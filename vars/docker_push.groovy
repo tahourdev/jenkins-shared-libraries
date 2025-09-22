2e40e5bf152aa12931a06246757e458f9ea51f23
Keanghor <72548356+tahourdev@users.noreply.github.com>
2025-09-22 22:39:07 +0700

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,15 +1,35 @@
-def call(String DockerHubRepo, String ImageTag){
-  echo "📦 Tagging and pushing Docker image to DockerHub..."
-  withCredentials([
-    usernamePassword(
-      credentialsId: 'docker-hub-credentials', 
-      usernameVariable: 'DOCKERHUB_USER', 
-      passwordVariable: 'DOCKERHUB_PASS')]){
+// def call(String DockerHubRepo, String ImageTag){
+//   echo "📦 Tagging and pushing Docker image to DockerHub..."
+//   withCredentials([
+//     usernamePassword(
+//       credentialsId: 'docker-hub-credentials', 
+//       usernameVariable: 'DOCKERHUB_USER', 
+//       passwordVariable: 'DOCKERHUB_PASS')]){
 
-    sh """
-      echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
-      docker push ${DockerHubRepo}:${ImageTag}
-      docker logout
-    """
-  }
+//     sh """
+//       echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
+//       docker push ${DockerHubRepo}:${ImageTag}
+//       docker logout
+//     """
+//   }
+// }
+
+
+def call(Map config) {
+    def image = "${config.dockerhubUser}/${config.appName}:${config.tag ?: 'latest'}"
+    echo "📦 Pushing Docker image: ${image} to DockerHub"
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'docker-hub-credentials',
+            usernameVariable: 'DOCKERHUB_USER',
+            passwordVariable: 'DOCKERHUB_PASS'
+        )
+    ]) {
+        sh """
+            echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
+            docker push ${image}
+            docker logout
+        """
+    }
 }
