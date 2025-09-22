2d0fdd14293ee7e46212f4d49f001a1246a77cef
Keanghor <72548356+tahourdev@users.noreply.github.com>
2025-09-22 22:38:18 +0700

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,11 @@
-def call(String DockerRepo, String ImageTag) {
-    echo "🔧 Building Docker image: ${DockerRepo}:${ImageTag}"
-    sh "docker build -t ${DockerRepo}:${ImageTag} ."
+// def call(String DockerRepo, String ImageTag) {
+//     echo "🔧 Building Docker image: ${DockerRepo}:${ImageTag}"
+//     sh "docker build -t ${DockerRepo}:${ImageTag} ."
+// }
+
+
+def call(Map config) {
+    def image = "${config.dockerhubUser}/${config.appName}:${config.tag ?: 'latest'}"
+    echo "🔧 Building Docker image: ${image}"
+    sh "docker build -t ${image} ${config.context ?: '.'}"
 }
