8797f2b1f3bd8296d5288b9aa18a91e160ceaf61
Keanghor <72548356+tahourdev@users.noreply.github.com>
2025-09-22 21:02:47 +0700

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def docker_build(String DockerRepo, String ImageTag) {
+def call(String DockerRepo, String ImageTag) {
     echo "🔧 Building Docker image: ${DockerRepo}:${ImageTag}"
     sh "docker build -t ${DockerRepo}:${ImageTag} ."
 }
